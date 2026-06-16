f8e96db5ca3ef0e438e35abc08f6ccc1017a3003
MiguelGarciaMartinezz <miguel.garcia4@edu.upct.es>
2026-06-16 16:48:30 +0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,12 +1,19 @@
+//creación de la app angular
 const app = angular.module('gestorApp', []);
 
+//creación del controlador de login
+//scope es el objeto que contiene los datos y funciones que se van a usar en la vista
+//window es el objeto que representa la ventana del navegador
+//http es el objeto que permite hacer peticiones HTTP al servidor
 app.controller('LoginController', function($scope, $window, $http) {
     $scope.usuario = {};
     $scope.mensajeError = "";
 
+    //función que se ejecuta al pulsar el boton de login
     $scope.hacerLogin = function() {
         if ($scope.usuario.user && $scope.usuario.passwd) {
             
+            //petición POST al servidor para hacer login POST es enviar get es param pedir información
             $http({
                 method: 'POST',
                 url: 'http://localhost:3000/login',
@@ -15,9 +22,11 @@ app.controller('LoginController', function($scope, $window, $http) {
                     passwd: $scope.usuario.passwd 
                 }
             }).then(function(response) {
+                //si el login es correcto, guardamos el token y el nombre de usuario en sessionStorage
                 $window.sessionStorage.setItem('token', response.data.session_id);
                 $window.sessionStorage.setItem('username', response.data.name);
                 
+                //redireccionamos a la página correspondiente según el rol del usuario
                 if (response.data.name === 'Administrador') {
                     $window.location.href = 'admin.html';
                 } else {
